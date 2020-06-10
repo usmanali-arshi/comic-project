017156356eb736a624b4e4f08a8ecd42e845ba18
sashanksilwal <sashanksilwal@hotmail.com>
2020-06-10 08:34:30 +0400

    added panel 1

--- js/script.js
+++ js/script.js
@@ -5,3 +5,31 @@ function openPage(pageUrl){
 function openPageNew(pageUrl){
 window.open(pageUrl);
 }
+
+
+// Panel 1
+var panelPArt = 0;
+
+document.addEventListener('keydown', logKey);
+
+function logKey(e) {
+  if (panelPArt==0 && e.which==32){
+    document.getElementById("panel1").src = "../images/ClassroomU3.png" ;
+    panelPArt++;
+    console.log(panelPArt);
+  }
+  else if (panelPArt==1 && e.which==32){
+    document.getElementById("panel1").src = "../images/ClassroomU3.png" ;
+      $('.dialogue1').fadeIn(2000);  
+    panelPArt++;
+    console.log(panelPArt);
+
+  }
+  else if (panelPArt == 2 && e.which==32){
+    document.getElementById("panel1").src = "../images/ClassroomU3.png" ;
+    panelPArt++;
+    console.log(panelPArt);
+
+  }
+}
+
