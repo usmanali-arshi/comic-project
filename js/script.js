055573e7119472795d01a7f3c8e342aafbc828d0
sashanksilwal <sashanksilwal@hotmail.com>
2020-06-10 18:07:07 +0400

    added text to panel1

--- js/script.js
+++ js/script.js
@@ -16,33 +16,41 @@ function logKey(e) {
   if (panelPArt==0 && e.which==32){
     document.getElementById("panel1").src = "../images/panel1/ClassroomU3.png" ;
     panelPArt++;
-    console.log(panelPArt);
+    
   }
   else if (panelPArt==1 && e.which==32){
     document.getElementById("panel1").src = "../images/panel1/ClassroomU3.png" ;
       $('#dialogue1').fadeIn(2000);  
+      $('#dialogue11').fadeIn(2000); 
+      $('#dialogue12').fadeIn(2000);
+      
     panelPArt++;
-    console.log(panelPArt);
+  
 
   }
   else if (panelPArt == 2 && e.which==32){
+    $('#dialogue11').fadeOut(0); 
+    $('#dialogue12').fadeOut(0);
     $('#dialogue2').fadeIn(2000); 
+
+    
     panelPArt++;
-    console.log(panelPArt);
+    
 
   }
   else if (panelPArt == 3 && e.which==32){
     $('#dialogue3').fadeIn(2000); 
     panelPArt++;
-    console.log(panelPArt);
+    
 
   }
-  else if (panelPArt == 4 && e.which==32){
-    $('#dialogue4').fadeIn(2000); 
-    panelPArt++;
-    console.log(panelPArt);
+  // else if (panelPArt == 4 && e.which==32){
+  //   $('#dialogue3').fadeIn(2000); 
+  //   panelPArt++;
+   
 
-  }
+  // }
+
   else if(panelPArt==5){
       alert("Go to next page");
   }
