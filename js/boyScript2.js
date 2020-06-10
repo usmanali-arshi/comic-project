cc7a37d3f29cf6949cf4848cfe561e66695cc5a5
sashanksilwal <sashanksilwal@hotmail.com>
2020-06-11 03:13:35 +0400

    uploaded panel 3

--- js/boyScript2.js
+++ js/boyScript2.js
@@ -25,32 +25,43 @@ function logKey(e) {
     $('#dialogue11').fadeOut(0); 
     $('#dialogue2').fadeIn(2000); 
     panel2Part++;
-    
-
   }
  
   else if (panel2Part == 3 && e.which==32){
     document.getElementById("panel3").src = "panel 3.png" ;
-    $('#dialogue3').fadeIn(2000); 
     panel2Part++;
-   
 
   }
   else if (panel2Part == 4 && e.which==32){
     $('#dialogue3').fadeIn(2000); 
     panel2Part++;
-    setTimeout(document.getElementById("end").style.display="block", 2000); 
-    
 
   }
   else if (panel2Part == 5 && e.which==32){
-    $('#dialogue3').fadeIn(2000); 
+    $('#dialogue3').fadeOut(0);
+    $('#dialogue4').fadeIn(2000); 
     panel2Part++;
+
+  }
+  else if (panel2Part == 6 && e.which==32){
+    $('#dialogue4').fadeOut(0);
+    $('#dialogue5').fadeIn(2000); 
+    document.getElementById("panel3").src = "panel 3 blush.png" ;
+    panel2Part++;
+
+  }
+  else if (panel2Part == 7 && e.which==32){
+    setTimeout(document.getElementById("end").style.display="block", 2000); 
+    // panel2Part++;
+
+  }
+
+//   else if (panel2Part == 5 && e.which==32){
+//     // $('#dialogue3').fadeIn(2000); 
+//     panel2Part++;
    
 
-  }
+//   }
 
-  else if(panel2Part==4){
-      alert("Go to next page");
-  }
+  
 }
